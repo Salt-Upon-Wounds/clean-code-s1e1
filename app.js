d4b0d780af2d2abc18afeac8cdc501ea0b5f30e5
Kirill <kiw197204@gmail.com>
2024-01-07 23:42:32 +0300

    fix(advanced-1.2): added alt for img in html and js files

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ var createNewTaskElement=function(taskString){
   deleteButton.className="btn delete";
   deleteButtonImg.src='./remove.svg';
   deleteButtonImg.className="btn__img";
+  deleteButtonImg.alt="remove";
   deleteButton.appendChild(deleteButtonImg);
 
 
